f5693cac0b0f00821f43f513184ce8714737b715
Fanny <fanny@MacBook-Pro-de-Adrien.local>
2018-07-31 19:24:25 +0200

    add last commit borders :cat:

--- js/colors.js
+++ js/colors.js
@@ -134,3 +134,35 @@ $('.randomBackgroundTitle').click(function () {
   $('.title').css('background-color', colorsBackground[random]);
   console.log(colorsBackground[random]);
 })
+
+// randomBorderColor
+var colorsBorders =
+[
+  '#fc5c65',
+  '#fd9644',
+  '#fed330',
+  '#26de81',
+  '#2bcbba',
+  '#eb3b5a',
+  '#fa8231',
+  '#f7b731',
+  '#20bf6b',
+  '#0fb9b1',
+  '#45aaf2',
+  '#4b7bec',
+  '#a55eea',
+  '#ffffff',
+  '#d1d8e0',
+  '#778ca3',
+  '#2d98da',
+  '#3867d6',
+  '#8854d0',
+  '#a5b1c2',
+  '#4b6584'
+];
+
+$('.randomBorderColor').click(function () {
+  var random = Math.floor(Math.random() * colorsBorders.length);
+  $('img').css('border-color', colorsBorders[random]);
+  console.log(colorsBorders[random]);
+})
